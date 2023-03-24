d8758bbf68b61cf4642945b9807a91b48d2d5922
Michael Zaverukha <zorkiz28@gmail.com>
2023-03-24 13:40:56 +0200

    dz23

---
+++ scripts/script.js
@@ -0,0 +1,72 @@
+const API = "6ba66b072f79ce23e1d45269b083438d";
+const mainCont = document.querySelector('.main-Cont');
+const inputeInfo = document.querySelector('.inpute-Info');
+const btn = document.querySelector('.btn');
+const result = document.querySelector('.result');
+console.log(API);
+async function getWeather(city){
+ const response =(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}&units=metric`);
+  const res = await fetch(`${response}`);
+    const data = await res.json();
+    console.log(data);
+   let temperature = `${Math.round(data.main.temp)}°C`;
+   let name = data.name;
+   let main = data.weather[0]['main'];
+   let image = '<img src="http://openweathermap.org/img/wn/' + data.weather[0]['icon'] + '@2x.png">';
+   let weatherName = document.createElement('p');
+   let weatherblockPhoto = document.createElement('div');
+   let weatherblockText = document.createElement('div');
+   let weathertTemp = document.createElement('p');
+   let weatherMain = document.createElement('p');
+   weatherblockText.style.display='flex';
+   weatherblockText.style.flexDirection = 'column';
+   weatherblockText.style.gap='10px';
+   weatherName.style.fontSize = '32px';
+   weathertTemp.style.fontSize = '24px';
+   weatherMain.style.fontSize = '16px';
+   result.appendChild(weatherblockText);
+   result.appendChild(weatherblockPhoto);
+   weatherblockText.appendChild(weatherName);
+   weatherblockText.appendChild(weathertTemp);
+   weatherblockText.appendChild(weatherMain);
+   localStorage.setItem(`Name`, name);
+   localStorage.setItem(`Temperature`, temperature);
+   localStorage.setItem(`Weather`, main);
+   localStorage.setItem(`Image`, image);
+   let getName = localStorage.getItem(`Name`);
+   let getTemp = localStorage.getItem(`Temperature`);
+   let getWeather = localStorage.getItem(`Weather`);
+   let getImg = localStorage.getItem(`Image`);
+   weatherblockPhoto.innerHTML = getImg;
+   weatherName.innerText = getName;
+   weathertTemp.innerText = getTemp;
+   weatherMain.innerText = getWeather;
+   setInterval(() => {
+    getWeather(city);
+    localStorage.clear();
+}, 7.2e+6);
+
+}
+
+window.addEventListener('load', () =>{
+  const town = localStorage.getItem(`City`);
+    if (town){
+      getWeather(city);
+    }
+});
+
+function displayWeather(){
+const city = (inputeInfo.value).trim();
+result.innerHTML = '';
+getWeather(city);
+};
+btn.addEventListener('click', displayWeather);
+
+
+window.addEventListener('load', () => {
+  const savedCity = localStorage.getItem('Name');
+  if (savedCity) {
+    inputeInfo.value = savedCity;
+    displayWeather();
+  }
+});
